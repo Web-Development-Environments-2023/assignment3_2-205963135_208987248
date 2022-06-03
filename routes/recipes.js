536385ff5a15dcecc0dc1de0e22a1772b48731c8
Maor <bitondor@post.bgu.ac.il>
2022-06-04 00:00:12 +0300

    fixing the get recipe details bug

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const recipes_utils = require("./utils/recipes_utils");
+const user_utils = require("./utils/user_utils")
 
 router.get("/", (req, res) => res.send("im here"));
 
@@ -8,10 +9,14 @@ router.get("/", (req, res) => res.send("im here"));
 /**
  * This path returns a full details of a recipe by its id
  */
-router.get("/details/:recipeId", async (req, res, next) => {
+router.get("/details/:recipeId/:userName", async (req, res, next) => {
   try {
-    const recipe = await recipes_utils.getRecipeDetails(req.params.recipeId);
+    let recipe_id = (req.params.recipeId).trim()
+    let user_name = (req.params.userName).trim()
+    // console.log(user_name);
+    const recipe = await recipes_utils.getRecipeDetails(recipe_id);
     res.send(recipe);
+    // user_utils.addWatchedRecipe(user_name, recipe_id);
   } catch (error) {
     next(error);
   }
