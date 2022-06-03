0081871128d639de163c093b96cac2bea5696451
Maor <bitondor@post.bgu.ac.il>
2022-06-03 23:13:12 +0300

    fixing the get recipes with the postman

--- routes/recipes.js
+++ routes/recipes.js
@@ -8,10 +8,19 @@ router.get("/", (req, res) => res.send("im here"));
 /**
  * This path returns a full details of a recipe by its id
  */
-router.get("/:recipeId", async (req, res, next) => {
+router.get("/details/:recipeId", async (req, res, next) => {
   try {
     const recipe = await recipes_utils.getRecipeDetails(req.params.recipeId);
     res.send(recipe);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/previewDetails", async (req, res, next) => {
+  try {
+    const recipes = await recipes_utils.getRecipesPreview(req.body.recipeIdList);
+    res.send(recipes);
   } catch (error) {
     next(error);
   }
